57a53a396d7d32d16e233fd3d2b514e5a04fcb50
Eisten Flores Sanchez <49214513+Eisten1996@users.noreply.github.com>
2021-06-18 11:07:56 -0500

    Initial commit

---
+++ src/main/java/pe/com/hiper/bmatic/perfilagendamientows/PerfilAgendamientoWsApplication.java
@@ -0,0 +1,13 @@
+package pe.com.hiper.bmatic.perfilagendamientows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfilAgendamientoWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerfilAgendamientoWsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/com/hiper/bmatic/perfilagendamientows/PerfilAgendamientoWsApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.hiper.bmatic.perfilagendamientows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfilAgendamientoWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
