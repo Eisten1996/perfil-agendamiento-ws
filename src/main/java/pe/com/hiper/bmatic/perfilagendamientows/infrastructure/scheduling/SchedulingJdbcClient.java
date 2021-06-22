e590f64eccf583c06bdb009403f565bcfff55ad9
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2021-06-22 15:28:46 -0500

    corriguiendo el guardar scheduling

--- src/main/java/pe/com/hiper/bmatic/perfilagendamientows/infrastructure/scheduling/SchedulingJdbcClient.java
+++ src/main/java/pe/com/hiper/bmatic/perfilagendamientows/infrastructure/scheduling/SchedulingJdbcClient.java
@@ -1,13 +1,18 @@
 package pe.com.hiper.bmatic.perfilagendamientows.infrastructure.scheduling;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 import pe.com.hiper.bmatic.perfilagendamientows.domain.scheduling.model.Scheduling;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Objects;
 
 @Component
 public class SchedulingJdbcClient {
@@ -48,13 +53,27 @@ public class SchedulingJdbcClient {
                     + "BMULTIPLE = ?, BCONFIRMAREMAIL = ?, NTIEMPOCONFIREMAIL = ?, CUNIDTIEMPOCONFEMAIL = ? "
                     + "WHERE NCODPERFILAGENDAMIENTO = ? ");
         }
-        jdbcTemplate.update(query.toString(), scheduling.getBranchId(), scheduling.getMinDays(),
-                scheduling.getMaxDays(), scheduling.getToleranceTime(), scheduling.getServices(),
-                scheduling.getMultipleBookings(), scheduling.getConfirmEmail(), scheduling.getConfirmTime(),
-                scheduling.getUnidConfirmTime(), scheduling.getId() == 0 ? new java.sql.Timestamp(System.currentTimeMillis())
-                        : scheduling.getId());
+        
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(query.toString(), new String[]{"id_usuario"});
+            ps.setString(1, scheduling.getBranchId());
+            ps.setInt(2, scheduling.getMinDays());
+            ps.setInt(3, scheduling.getMaxDays());
+            ps.setInt(4, scheduling.getToleranceTime());
+            ps.setString(5, scheduling.getServices());
+            ps.setInt(6, scheduling.getMultipleBookings());
+            ps.setInt(7, scheduling.getConfirmEmail());
+            ps.setInt(8, scheduling.getConfirmTime());
+            ps.setString(9, scheduling.getUnidConfirmTime());
+            if (scheduling.getId() == 0) {
+                ps.setTimestamp(10, new java.sql.Timestamp(System.currentTimeMillis()));
+            } else {
+                ps.setInt(10, scheduling.getId());
+            }
+            return ps;
+        }, keyHolder);
         if (scheduling.getId() == 0) {
-            schedulingId = keyHolder.getKey().intValue();
+            schedulingId = Objects.requireNonNull(keyHolder.getKey()).intValue();
         } else {
             schedulingId = scheduling.getId();
         }
